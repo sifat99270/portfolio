a91b0b8ed243ebce826f43c5e43beea278576262
smn <rasifat33@gmail.com>
2024-02-05 19:49:14 +0600

    last

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,24 +8,7 @@ import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export async function generateMetadata() {
 
-  const res = await fetch(`${process.env.HOST}/api/graph`);
-  console.log(res)
-  const result = await res.json()
-  console.log(result)
-  if (result['status'] === 'success') {
-    return {
-      metadataBase: new URL(process.env.HOST),
-      title: result['data']['title'],
-      description: result['data']['description'],
-      keywords: result['data'][' keywords'],
-
-      openGraph: result['data']['openGraph']
-    }
-  }
-  return;
-}
 
 
 export default function RootLayout({ children }) {
